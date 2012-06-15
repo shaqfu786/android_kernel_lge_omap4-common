c6b53fa741443320a07637e97a16a21996d12d68
Suman Anna <s-anna@ti.com>
2012-06-14 21:42:23 -0500

    Revert "omap4: sdp/panda/tablet: reserve memory for dsp"
    This reverts commit e699cc2c4cc3f369108d83370172138294f9840f.
    
    DSP currently requires 4MB of memory to be set-aside to meet its
    firmware requirements and can use either a fixed carveout or
    a dynamically assigned carveout. The above commit uses static
    carveouts for DSP in various board files. DSP does not have a
    mandatory requirement to use this fixed carveout, and has been
    switched over to use a dynamically allocated carveout address.
    The above commit is therefore being reverted to free up unused
    carveout memory.
    
    Change-Id: If080fa45cc8d81f0540a584902fe29b590fb207c
    Signed-off-by: Jesse Villarreal <jesse.villarreal@ti.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>
    
    Conflicts:
    	arch/arm/mach-omap2/include/mach/omap4_ion.h

--- arch/arm/mach-omap2/include/mach/omap4_ion.h
+++ arch/arm/mach-omap2/include/mach/omap4_ion.h
@@ -17,31 +17,15 @@
 #ifndef _OMAP4_ION_H
 #define _OMAP4_ION_H
 
-#include <linux/ion.h>
-
 #define OMAP4_ION_HEAP_SECURE_INPUT_SIZE	(SZ_1M * 90)
+#define OMAP4_ION_HEAP_TILER_SIZE		(SZ_128M - SZ_32M)
+#define OMAP4_ION_HEAP_NONSECURE_TILER_SIZE	SZ_32M
 
 #define PHYS_ADDR_SMC_SIZE	(SZ_1M * 3)
 #define PHYS_ADDR_SMC_MEM	(0x80000000 + SZ_1G - PHYS_ADDR_SMC_SIZE)
 #define PHYS_ADDR_DUCATI_SIZE	(SZ_1M * 105)
 #define PHYS_ADDR_DUCATI_MEM	(PHYS_ADDR_SMC_MEM - PHYS_ADDR_DUCATI_SIZE - \
 				OMAP4_ION_HEAP_SECURE_INPUT_SIZE)
-
-#ifdef CONFIG_OMAP_REMOTE_PROC_DSP
-#define PHYS_ADDR_TESLA_SIZE	(SZ_1M * 4)
-#define PHYS_ADDR_TESLA_MEM	(PHYS_ADDR_DUCATI_MEM - \
-					OMAP4_ION_HEAP_TILER_SIZE - \
-					PHYS_ADDR_TESLA_SIZE)
-#endif
-
-
-struct omap_ion_platform_data {
-	struct ion_platform_data *ion;
-	u32 tiler2d_size;
-	u32 nonsecure_tiler2d_size;
-};
-
-struct omap_ion_platform_data *get_omap_ion_platform_data(void);
 
 #ifdef CONFIG_ION_OMAP
 void omap_ion_init(void);
