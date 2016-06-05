968fc3ec40b0eda2198be7f97114dd598ffade1e
shaqfu786 <baileyms@telkomsa.net>
2016-06-05 13:53:36 +0200

    Revert "fs: introduce inode operation ->update_time"
    This reverts commit 8200490788a76e1aa785b941da699793f27d47a9.

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1019,9 +1019,7 @@ static ssize_t fuse_file_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	if (err)
 		goto out;
 
-	err = file_update_time(file);
-	if (err)
-		goto out;
+	file_update_time(file);
 
 	iov_iter_init(&i, iov, nr_segs, count, 0);
 	written = fuse_perform_write(file, mapping, &i, pos);
